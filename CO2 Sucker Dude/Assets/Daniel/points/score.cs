f9e2ea67b6d0b64ff8e6f7d330fbbc8b6ddd8e82
DanCpp12 <daniel@esteen.st>
2023-11-18 12:06:10 +0100

    score system

---
+++ CO2 Sucker Dude/Assets/Daniel/points/score.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Score", order = 1)]
+public class score : ScriptableObject
+{
+    public int Score = 0;
+}
