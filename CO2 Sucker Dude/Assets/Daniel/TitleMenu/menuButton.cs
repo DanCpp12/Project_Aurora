00a1d7bf0fdb51e44ba70bf659f956dca8ac5bda
DanCpp12 <daniel@esteen.st>
2023-11-18 21:30:58 +0100

    TitleMenu

---
+++ CO2 Sucker Dude/Assets/Daniel/TitleMenu/menuButton.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuButton : MonoBehaviour
+{
+    public void StartBtn()
+    {
+        SceneManager.LoadSceneAsync("MalinMainScene");
+    }
+    public void ExitBtn()
+    {
+        Application.Quit();
+    }
+}
