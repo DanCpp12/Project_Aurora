4c954d966834b0f6fa7c2391340db6e73e777c36
linsall <linus.salomonsson19@gmail.com>
2024-04-04 14:57:24 +0200

    Updated Damage variable
    Woopsies accidentally put the variable in private instead of public

--- Source/Project_Aurora/Project_AuroraCharacter.h
+++ Source/Project_Aurora/Project_AuroraCharacter.h
@@ -22,6 +22,9 @@ public:
 	/** Returns CameraBoom subobject **/
 	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Damage = 20.f;
+
 private:
 	/** Top down camera */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
@@ -32,7 +35,6 @@ private:
 	UPROPERTY(VisibleAnywhere)
 	class UHealthComponent* HealthComponent = nullptr;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	float Damage = 20.f;
+
 };
 
