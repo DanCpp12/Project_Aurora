306eede246a7c6d0f0f1756047a6f4863f4a3001
DanCpp12 <144418307+DanCpp12@users.noreply.github.com>
2024-04-28 13:43:42 +0200

    added combat AI. improved movement AI
    added randomised combat state. added timer to next movement, AI can only move so far  from spawn point

--- Source/Project_Aurora/AIComponent.cpp
+++ Source/Project_Aurora/AIComponent.cpp
@@ -1,6 +1,7 @@
 #include "AIComponent.h"
 #include "Components/SphereComponent.h"
 #include "GameFramework/Character.h"
+#include "Math/UnrealMathUtility.h"
 
 UAIComponent::UAIComponent()
 {
@@ -21,17 +22,77 @@ void UAIComponent::BeginPlay()
 	Sphere->AttachToComponent(GetOwner()->GetRootComponent(), FAttachmentTransformRules::KeepRelativeTransform);
 	GetOwner()->FinishAndRegisterComponent(Sphere);
 	Sphere->SetupAttachment(GetOwner()->GetRootComponent());
+
+	SpawnPoint = GetOwner()->GetActorLocation();
 }
 
-void UAIComponent::base_EnemyMovment()
+FVector UAIComponent::base_EnemyMovmentAI()
 {
-	
+	if ((int)GetOwner()->GetActorLocation().X <= (int)target.X + 60 && (int)GetOwner()->GetActorLocation().Y <= (int)target.Y + 60 &&
+		(int)GetOwner()->GetActorLocation().X >= (int)target.X - 60 && (int)GetOwner()->GetActorLocation().Y >= (int)target.Y - 60)
+	{
+		if (MovmentState == MovmentStates::Idle && !NewTarget)
+		{
+			MTimer = 0;
+		}
+		NewTarget = true;
+	}
+	Base_EnemyMovmentState();
+
+	if (MovmentState == MovmentStates::Attack)
+	{
+		NewTarget = false;
+		//UE_LOG(LogTemp, Log, TEXT("Attack actor att location: %f, %f, %f"), target.X, target.Y, target.Z);
+	}
+	else if (MovmentState == MovmentStates::Idle && NewTarget)
+	{
+		if (MovmentTimer <= MTimer)
+		{
+			NewTarget = false;
+			float angle = FMath::RandRange(-45.f, 45.f);
+			angle += GetOwner()->GetActorRotation().Yaw;
+			FVector offset = FVector(cosf(angle), sinf(angle), 0) * MovmentRange;
+			if (FVector::DistXY(GetOwner()->GetActorLocation() + offset, SpawnPoint) <= MovmentRadiusFromSpawnPoint)
+			{
+				target = GetOwner()->GetActorLocation() + offset;
+				target.Z = 100.f;
+				UE_LOG(LogTemp, Log, TEXT("move to random position: %f, %f, %f"), target.X, target.Y, target.Z);
+			}
+		}
+		else
+		{
+			MTimer += GetWorld()->DeltaTimeSeconds;
+		}
+		
+	}
+	return target;
+}
+
+bool UAIComponent::base_EnemyCombatAI()
+{
+	if (MovmentState != MovmentStates::Attack)
+	{
+		CombatState = CombatStates::Null;
+		UE_LOG(LogTemp, Log, TEXT("combat state = null"));
+		return false;
+	}
+	else
+	{
+		Base_EnemyCombatState();
+		if (CombatState == CombatStates::Attack)
+		{
+			UE_LOG(LogTemp, Log, TEXT("Hit enemy:"));
+		}
+		else if (CombatState == CombatStates::Block)
+		{
+			UE_LOG(LogTemp, Log, TEXT("Block enemy:"));
+		}
+		return true;
+	}
 }
 
 void UAIComponent::HandleBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	UE_LOG(LogTemp, Log, TEXT("Begin Overlap: %s"), *OtherActor->GetName());
-
 	for (int i = 0; i < 10; ++i)
 	{
 		if (Targets[i] == nullptr)
@@ -40,11 +101,11 @@ void UAIComponent::HandleBeginOverlap(UPrimitiveComponent* OverlappedComponent, 
 			return;
 		}
 	}
+	base_EnemyMovmentAI();
 }
 
 void UAIComponent::HandleEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
-	UE_LOG(LogTemp, Log, TEXT("End Overlap: %s"), *OtherActor->GetName());
 	for (int i = 0; i < 10; i++)
 	{
 		if (OtherActor == Targets[i])
@@ -52,5 +113,35 @@ void UAIComponent::HandleEndOverlap(UPrimitiveComponent* OverlappedComponent, AA
 			Targets[i] = nullptr;
 		}
 	}
+	base_EnemyMovmentAI();
 }
 
+void UAIComponent::Base_EnemyMovmentState()
+{
+	for (int i = 1; i < 10; ++i)
+	{
+		if (Targets[i] != nullptr)
+		{
+			MovmentState = MovmentStates::Attack;
+			target = Targets[i]->GetActorLocation();
+			break;
+		}
+		else
+		{
+			MovmentState = MovmentStates::Idle;
+		}
+	}
+}
+
+void UAIComponent::Base_EnemyCombatState()
+{
+	int randstate = FMath::RandRange(1, 2);
+	if (randstate == 1)
+	{
+		CombatState = CombatStates::Attack;
+	}
+	else if (randstate == 2)
+	{
+		CombatState = CombatStates::Block;
+	}
+}
--- Source/Project_Aurora/AIComponent.h
+++ Source/Project_Aurora/AIComponent.h
@@ -4,6 +4,22 @@
 
 class USphereComponent;
 
+UENUM(BlueprintType)
+enum class MovmentStates
+{
+	Null,
+	Idle,
+	Attack
+};
+
+UENUM(BlueprintType)
+enum class CombatStates
+{
+	Null,
+	Attack,
+	Block
+};
+
 UCLASS(meta = (BlueprintSpawnableComponent))
 class UAIComponent : public UActorComponent
 {
@@ -13,7 +29,11 @@ public:
 	UAIComponent();
 	void BeginPlay() override;
 
-	void base_EnemyMovment();
+	UFUNCTION(BlueprintCallable)
+	FVector base_EnemyMovmentAI();
+
+	UFUNCTION(BlueprintCallable)
+	bool base_EnemyCombatAI();
 
 	UFUNCTION()
 	void HandleBeginOverlap(
@@ -29,11 +49,36 @@ public:
 		UPrimitiveComponent* OtherComp,
 		int32 OtherBodyIndex);
 
+	UPROPERTY(BlueprintReadWrite)
+	bool NewTarget = true;
+	
+
 private:
 	UPROPERTY(EditDefaultsOnly)
 	USphereComponent* Sphere = nullptr;
 
+	UPROPERTY(Editanywhere)
+	int MovmentRange = 300;
+
+	UPROPERTY(EditDefaultsOnly/*, VisibleAnywhere*/)
+	int MovmentRadiusFromSpawnPoint = 1000;
+
+	UPROPERTY(Editanywhere/*, VisibleAnywhere*/)
+	float MovmentTimer;
+
 	UPROPERTY(VisibleAnywhere)
 	AActor* Targets[10] = { nullptr };
+	UPROPERTY(VisibleAnywhere)
+	FVector target;
 
+	UPROPERTY(VisibleAnywhere)
+	MovmentStates MovmentState;
+
+	UPROPERTY(VisibleAnywhere)
+	CombatStates CombatState;
+
+	FVector SpawnPoint;
+	float MTimer = 0;
+	void Base_EnemyMovmentState();
+	void Base_EnemyCombatState();
 };
